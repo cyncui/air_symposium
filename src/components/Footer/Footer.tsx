5b7f751662d1f00b281cd45f49e791e246f3e190
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-05-28 23:38:10 -0700

    fix: aligned footer to 7col grid

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
     <footer className="py-32 bg-stone-100 text-background">
-      <div className="px-7.5 mx-auto grid grid-cols-4 lg:grid-cols-6 gap-8">
-        <div className="max-sm:col-span-full sm:col-span-2">
+      <div className="px-7.5 mx-auto grid grid-cols-7 gap-4">
+        <div className="max-sm:col-span-full sm:col-span-3 lg:col-span-2">
           <p className="mb-4">
             We acknowledge that affiliates and the team behind of AIR are
             residing on stolen land of the səl̓ilw̓ətaʔɬ (Tsleil-Waututh),
@@ -15,7 +15,7 @@ export const Footer = () => {
             back!
           </p>
         </div>
-        <div className="flex gap-2 col-span-full sm:col-span-2 row-end-auto text-xs">
+        <div className="flex gap-2 col-span-full sm:col-span-3 lg:col-span-2 row-end-auto text-xs">
           <div className="size-8 rounded-full bg-foreground flex-none" />
           <div>
             <p>
